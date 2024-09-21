820a81097c08deaf32cc25feac9b2eab60037307
SlayerSv <slayer-sv@mail.ru>
2024-09-21 20:31:09 +0700

    [sysprog]: [practice1]: feat: add main file which is the parent process.

---
+++ System-programming/practice1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    printf("Enter first value: ");
+    char input1[100];
+    scanf("%s", input1);
+    printf("Enter second value: ");
+    char input2[100];
+    scanf("%s", input2);
+
+    pid_t childPid;
+    char path[] = "child.out";
+    char* args[] = {path, input1, input2, NULL};
+    childPid = fork();
+    if (childPid == 0) {
+        execv(path, args);
+        printf("failed to run process\n");
+        return 1;
+    } else {
+        printf("Parent process. Child id is %d\n", childPid);
+        int child;
+        int id = wait(&child);
+        printf("Done waiting %d status %d\n", id, child);
+    }
+
+}
