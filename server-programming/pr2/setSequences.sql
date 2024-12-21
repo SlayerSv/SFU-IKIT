dd32743426473dee3eab53e0a1c064404ce3bfb1
SlayerSv <slayer-sv@mail.ru>
2024-12-21 20:32:29 +0700

    [server]: [pr2]: fix: remove unused file (script moved to import.sql file)

--- server-programming/pr2/setSequences.sql
+++
@@ -1,3 +0,0 @@
-SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(id),0) + 1, false) FROM users;
-SELECT setval(pg_get_serial_sequence('threads', 'id'), coalesce(max(id),0) + 1, false) FROM threads;
-SELECT setval(pg_get_serial_sequence('posts', 'id'), coalesce(max(id),0) + 1, false) FROM posts;
