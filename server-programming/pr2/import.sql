5d01eec01957312ac2a1becc1d76f195d5014b35
SlayerSv <slayer-sv@mail.ru>
2024-12-21 20:30:25 +0700

    [server]: [pr2]: feat: add script for importing db from csv files

---
+++ server-programming/pr2/import.sql
@@ -0,0 +1,31 @@
+delete from users;
+delete from friends;
+delete from threads;
+delete from posts;
+delete from likes;
+
+copy users
+from 'C:\Users\Public\documents\users.csv'
+delimiter ',';
+
+copy friends
+from 'C:\Users\Public\documents\friends.csv'
+delimiter ',';
+
+copy threads
+from 'C:\Users\Public\documents\threads.csv'
+delimiter ',';
+
+copy posts
+from 'C:\Users\Public\documents\posts.csv'
+delimiter ',';
+
+copy likes
+from 'C:\Users\Public\documents\likes.csv'
+delimiter ',';
+
+SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(id),0) + 1, false) FROM users;
+SELECT setval(pg_get_serial_sequence('threads', 'id'), coalesce(max(id),0) + 1, false) FROM threads;
+SELECT setval(pg_get_serial_sequence('posts', 'id'), coalesce(max(id),0) + 1, false) FROM posts;
+
+
