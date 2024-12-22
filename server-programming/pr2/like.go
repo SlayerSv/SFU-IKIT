27922f117efb36395a677c34b3e593cb7a3fa520
SlayerSv <slayer-sv@mail.ru>
2024-12-22 07:37:45 +0700

    [server]: [pr2]: fix: add check for existing likes

--- server-programming/pr2/like.go
+++ server-programming/pr2/like.go
@@ -14,13 +14,29 @@ type Like struct {
 
 var likes = []Like{}
 var errNoPosts = errors.New("no posts")
+var liked []map[int]bool
 
 func genLike(t time.Time) error {
+	if liked == nil {
+		liked = make([]map[int]bool, len(users))
+	}
 	if len(posts) == 0 {
 		return errNoPosts
 	}
-	user := rand.IntN(len(users))
-	post := rand.IntN(len(posts))
+	user, post := 0, 0
+	for {
+		user = rand.IntN(len(users))
+		post = rand.IntN(len(posts))
+		if liked[user] == nil {
+			liked[user] = make(map[int]bool)
+		}
+		_, alreadyLiked := liked[user][post]
+		if alreadyLiked {
+			continue
+		}
+		liked[user][post] = true
+		break
+	}
 	like := Like{
 		userID:     user,
 		postID:     post,
