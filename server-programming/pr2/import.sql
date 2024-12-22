59f4841e41f2da8ffd8196ad31b2f2027922602f
SlayerSv <slayer-sv@mail.ru>
2024-12-22 07:38:35 +0700

    [server]: [pr2]: fix: remove extra whitespace

--- server-programming/pr2/import.sql
+++ server-programming/pr2/import.sql
@@ -27,5 +27,3 @@ delimiter ',';
 SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(id),0) + 1, false) FROM users;
 SELECT setval(pg_get_serial_sequence('threads', 'id'), coalesce(max(id),0) + 1, false) FROM threads;
 SELECT setval(pg_get_serial_sequence('posts', 'id'), coalesce(max(id),0) + 1, false) FROM posts;
-
-
