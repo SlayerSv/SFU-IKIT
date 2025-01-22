ed9e0021ced3ce1245042ac3706a69a49ede591b
SlayerSv <slayer-sv@mail.ru>
2025-01-23 05:05:46 +0700

    [functional]: [lr3]: feat:  add get by types func, refactor get by type get fields, add mock data

--- functional-programming/lr3/db.scala
+++ functional-programming/lr3/db.scala
@@ -42,26 +42,75 @@ class DB(data: Map[String, values.DBVal] = Map[String, values.DBVal]()) {
             None
     }
 
-    def getByType(valTypeName: String): Option[List[String]] = {
-        val opt = values.ValType(valTypeName)
-        if opt.isEmpty then return None
-        val namesList = scala.collection.mutable.ListBuffer[String]()
-        val targetValType = opt.get
-        for (name, dbVal) <- data
-            if dbVal.isType(targetValType)
-        do
-            namesList += name
-        Some(namesList.toList)
+    def getByType(valTypeName: String): Optional[List[String]] = {
+        getByTypes(List(valTypeName))
     }
 
-    def getFields(name: String): Option[List[values.DBVal]] = {
-        val opt = data.get(name)
-        if opt.isEmpty then return None
-        val dbVal = opt.get
-        if dbVal.isStructured() then
-            val dbStructVal = dbVal.asInstanceOf[values.DBStructVal]
-            Option(dbStructVal.fields)
-        else 
+    def getByTypes(typeNames: List[String]): Optional[List[String]] = {
+        val namesList = scala.collection.mutable.ListBuffer[String]()
+        val valTypes = scala.collection.mutable.Set[values.ValType]()
+        for typeName <- typeNames
+        do
+            val opt = values.ValType(typeName)
+            if opt.isDefined then
+                valTypes += opt.get
+
+        if valTypes.isEmpty then
+            return None
+
+        for (name, dbVal) <- data
+        do
+            for valType <- valTypes
+            if dbVal.isType(valType)
+            do
+                namesList += name
+
+        if namesList.isEmpty then
             None
+        else
+            Some(namesList.toList)
+    }
+
+    def getFields(name: String): Option[List[(String, String)]] = {
+        val opt = data.get(name)
+        if opt.isEmpty then
+            return None
+        
+        val dbVal = opt.get
+        if !dbVal.isStructured() then
+            return None
+        
+        val dbStructVal = dbVal.asInstanceOf[values.DBStructVal]
+        val fieldsList = scala.collection.mutable.ListBuffer[(String,String)]()
+        for field <- dbStructVal.fields
+        do
+            fieldsList += (field.name, field.valType.typeName)
+
+        Some(fieldsList.toList)
+    }
+
+    def isStructed(name: String): Optional[Boolean] = {
+        val opt = data.get(name)
+        if opt.isEmpty then
+            return None
+
+        Some(opt.get.isStructured())
     }
 }
+
+val dataD = Map[String, values.DBVal](
+    ("id", values.DBVal("id", values.ValType.Int)),
+    ("name", values.DBVal("name", values.ValType.String)),
+    ("price", values.DBVal("price", values.ValType.Float)),
+    ("address", values.DBStructVal("address", List(
+        values.DBVal("house", values.ValType.Int),
+        values.DBVal("city", values.ValType.String),
+        values.DBVal("street", values.ValType.String)))),
+    ("person", values.DBStructVal("person", List(
+        values.DBVal("first name", values.ValType.String),
+        values.DBVal("last name", values.ValType.String),
+        values.DBVal("age", values.ValType.Int)))),
+    ("weight", values.DBVal("weight", values.ValType.Float)),
+    ("text", values.DBVal("text", values.ValType.String)),
+    ("amount", values.DBVal("amount", values.ValType.Int)),
+)
