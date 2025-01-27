d04950b08aab0cae1a18df86636419c2d8c4e501
SlayerSv <slayer-sv@mail.ru>
2025-01-28 06:56:08 +0700

    [testing]: [pr4]: feat: add chess move string method. Remove is castle fields, add checks for castling in string method

--- testing/pr4/chess_move.go
+++ testing/pr4/chess_move.go
@@ -1,10 +1,37 @@
 package pr4
 
+import "strings"
+
 type chessMove struct {
-	oldPos        chessBoardPosition
-	newPos        chessBoardPosition
-	pieceType     chessPieceType
-	isTake        bool
-	isShortCastle bool
-	isLongCastle  bool
+	oldPos    chessBoardPosition
+	newPos    chessBoardPosition
+	pieceType chessPieceType
+	isTake    bool
+	isCheck   bool
+	isMate    bool
+}
+
+// returns a long algebraic notation of the move to eliminate ambiguity
+func (cm chessMove) String() string {
+	var b strings.Builder
+	b.WriteString(cm.pieceType.String())
+	b.WriteString(cm.oldPos.String())
+	if cm.isTake {
+		b.WriteByte('x')
+	}
+	b.WriteString(cm.newPos.String())
+	if cm.isCheck {
+		b.WriteByte('+')
+	}
+	if cm.isMate {
+		b.WriteByte('#')
+	}
+	move := b.String()
+	if move == "Ke1g1" || move == "Ke8g8" {
+		return "0-0"
+	}
+	if move == "Ke1c1" || move == "Ke8c8" {
+		return "0-0-0"
+	}
+	return move
 }
