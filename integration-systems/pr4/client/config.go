7f008d4d68b0e4e16aa57237d84d09c7a71e2f8d
SlayerSv <slayer-sv@mail.ru>
2025-04-27 13:34:34 +0700

    [integration]: [pr4]: feat: add get api key func

--- integration-systems/pr4/client/config.go
+++ integration-systems/pr4/client/config.go
@@ -6,18 +6,19 @@ import (
 )
 
 type Config struct {
-	ServerAddr string `json:"server_addr"`
-	KafkaAddr  string `json:"kafka_addr"`
-	KafkaTopic string `json:"kafka_topic"`
+	ServerAddr   string `json:"server_addr"`
+	ServerAPIKey string `json:"server_api_key"`
+	KafkaAddr    string `json:"kafka_addr"`
+	KafkaTopic   string `json:"kafka_topic"`
 }
 
 func NewConfig() (Config, error) {
-	cfgFile, err := os.Open("config.json")
+	cfgFile, err := os.Open("client/config.json")
 	var cfg Config
 	if err != nil {
 		return cfg, err
 	}
-	err = json.NewEncoder(cfgFile).Encode(&cfg)
+	err = json.NewDecoder(cfgFile).Decode(&cfg)
 	if err != nil {
 		return cfg, err
 	}
--- integration-systems/pr4/client/main.go
+++ integration-systems/pr4/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -10,16 +11,19 @@ import (
 	"integration/pr4/broker/kafka"
 )
 
+var NewCurrencies []broker.Message
+
 func main() {
 	cfg, err := NewConfig()
 	if err != nil {
 		log.Fatalf("ERROR: Read config: %v", err)
 	}
+	cfg.ServerAPIKey = GetApiKey(cfg.ServerAddr)
 	kr := kafka.NewReader(cfg.KafkaAddr, cfg.KafkaTopic)
-	ListenAndRead(cfg.KafkaAddr, kr)
+	ListenAndRead(cfg.ServerAddr, cfg.ServerAPIKey, kr)
 }
 
-func ListenAndRead(serverAddr string, br broker.Reader) {
+func ListenAndRead(serverAddr, server_api_key string, br broker.Reader) {
 	for {
 		msg, err := br.Read()
 		if err != nil {
@@ -27,12 +31,13 @@ func ListenAndRead(serverAddr string, br broker.Reader) {
 			continue
 		}
 		log.Printf("INFO: Received broker message %s topic %s", msg, br.GetTopic())
-		GetCurrency(serverAddr, msg.Value)
+		NewCurrencies = append(NewCurrencies, msg)
+		GetCurrency(serverAddr, server_api_key, msg.Value)
 	}
 }
 
-func GetCurrency(serverAddr, code string) {
-	url := fmt.Sprintf("%s/currencies/%s", serverAddr, code)
+func GetCurrency(serverAddr, serserver_api_key, code string) {
+	url := fmt.Sprintf("http://%s/currencies/%s?api_key=%s", serverAddr, code, serserver_api_key)
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Printf("ERROR: Get currency from server: %v", err)
@@ -43,5 +48,32 @@ func GetCurrency(serverAddr, code string) {
 		log.Printf("ERROR: Read response body from server: %v", err)
 		return
 	}
+	if resp.StatusCode != 200 {
+		log.Printf("ERROR: Get currency from server code: %d body: %s", resp.StatusCode, string(msg))
+		return
+	}
 	log.Printf("INFO: Received currency from server:\n%s", string(msg))
+}
+
+func GetApiKey(serverAddr string) string {
+	url := fmt.Sprintf("http://%s/api_key", serverAddr)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("ERROR: Get api key from server: %v", err)
+		return ""
+	}
+	decoder := json.NewDecoder(resp.Body)
+	var APIKey struct {
+		APIKey string `json:"api_key"`
+	}
+	err = decoder.Decode(&APIKey)
+	if err != nil {
+		log.Printf("ERROR: decode api key: %v", err)
+		return ""
+	}
+	if resp.StatusCode != 201 {
+		log.Printf("ERROR: Get api key from server code: %d", resp.StatusCode)
+		return ""
+	}
+	return APIKey.APIKey
 }
